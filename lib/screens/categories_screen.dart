d893dfddd16c6e3bd8a4a00e43737f5a0ef4940b
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-03-07 00:11:23 -0300

    Adição da tela inicial

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,1 +1,26 @@
+import 'package:comidas/components/category_item.dart';
+import 'package:comidas/models/data/dummy_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar?'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
