7dee458332f91101d51f80073e8741ce02136080
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-03-10 12:27:34 -0300

    informações entre telas

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,14 +1,17 @@
+import 'package:comidas/models/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+  const CategoriesMealsScreen(this.category);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receitas'),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Receitas por categorias'),
+        child: Text('Receitas por categorias ${category.id}'),
       ),
     );
   }
