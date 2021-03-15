1df9ce118814288325081efc039bdbaae2ea294a
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-03-15 11:18:38 -0300

    Adição das etapas da receita

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -2,6 +2,31 @@ import 'package:comidas/models/meals.dart';
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  _creatSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(Widget child) {
+    return Container(
+      width: 330,
+      height: 200,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context).settings.arguments as Meal;
@@ -9,34 +34,20 @@ class MealDetailScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(meal.title),
         ),
-        body: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                meal.imageUrl,
-                fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'Ingredientes',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ),
-            Container(
-              width: 300,
-              height: 200,
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ListView.builder(
+              _creatSectionTitle(context, 'Ingredientes'),
+              _createSectionContainer(
+                ListView.builder(
                   itemCount: meal.ingredients.length,
                   itemBuilder: (ctx, index) {
                     return Card(
@@ -49,9 +60,28 @@ class MealDetailScreen extends StatelessWidget {
                       ),
                       color: Theme.of(context).accentColor,
                     );
-                  }),
-            )
-          ],
+                  },
+                ),
+              ),
+              _creatSectionTitle(context, 'Passos'),
+              _createSectionContainer(ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(meal.steps[index]),
+                      ),
+                      Divider(), //adiciona linha entre as etapas
+                    ],
+                  );
+                },
+              ))
+            ],
+          ),
         ));
   }
 }
