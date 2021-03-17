cee5894e21ca264bce03a626e717fcd8209df462
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-03-17 10:35:04 -0300

    Ajuste para destruir as telas evitando pilha de telas

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -50,12 +50,13 @@ class MainDrwaer extends StatelessWidget {
           _createItem(
             Icons.restaurant,
             'Refeições',
-            () => Navigator.of(context).pushNamed(AppRoutes.HOME),
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.HOME),
           ),
           _createItem(
             Icons.settings,
             'Configurações',
-            () => Navigator.of(context).pushNamed(AppRoutes.SETTINGS),
+            () =>
+                Navigator.of(context).pushReplacementNamed(AppRoutes.SETTINGS),
           ),
         ],
       ),
